0784b54f57419e3a1e730124953e3aacbc0eb0d6
Lana <alanaa5836@gmail.com>
2025-11-14 16:16:57 -0300

    Update output message to specify dollars

--- 010.py
+++ 010.py
@@ -1,2 +1,2 @@
 rs=float(input("Quantos reais carla tem? "))
-print(f"Se carla tem {rs} reais, ela pode comprar {rs/5.29:.2f}")
+print(f"Se carla tem {rs} reais, ela pode comprar {rs/5.29:.2f} dolares")
